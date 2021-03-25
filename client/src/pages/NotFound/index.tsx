e09540350d3285539888dd09318b6ab960fa35c4
Sukaato <sukaato.dev@gmail.com>
2021-03-25 21:35:00 +0100

    ✨ Add 404 page

---
+++ client/src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import './style.scss';
+
+export function NotFound() {
+
+  return (
+    <div id='notFound'>
+      <h1>NotFound</h1>
+      <p>La page demandé n'éxiste pas ou plu</p>
+    </div>
+  );
+}
